1a47146dac6be8a5b6380c7a43dee3bc1f485321
FrederikOlesen <Frederik.o@mailme.dk>
2014-10-22 11:37:55 +0200

    Added is it friday.

--- public/javascripts/todaysDate.js
+++ public/javascripts/todaysDate.js
@@ -2,9 +2,19 @@
  * Created by frederikolesen on 21/10/14.
  */
 
-function dateToday(){
+onload = function dateToday(){
     var date = new Date();
     var day = date.getDay();
     weekdays =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    alert(weekdays[day]);
+    var x = weekdays[day]
+
+    if(x === 'Friday'){
+        document.write('Holy moly shitballs. Its fucking friday' + x)
+        document.write("<img src='../images/happySmiley.jpg' />")
+    }
+
+    else{
+        document.write('Today its ' + x + ' and sadly not friday :(')
+        document.write("<img src='../images/sadSmiley.png' />")
+    }
 }
